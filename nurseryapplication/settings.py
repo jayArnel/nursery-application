a8b6637ba8ec72f91243f3a324eec20f7e1512f6
Jay Arnel Bilocura <jay.arnel@gmail.com>
2015-05-20 07:23:13 +0800

    startproject

--- nurseryapplication/settings.py
+++ nurseryapplication/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-5!*96qsgksm%xcv3cb6e%rbi7b%5bcl&(4&7*5k5hzxs7izan'
+SECRET_KEY = 'z-piiph2!vyh2*c1i@h+%t2*tcy3rs5j9@n^u8j&wkvy4vm!v5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
