9b5217adb6df5cad52fad33f8932b0c11d7abb24
Jay Arnel Bilocura <jay.arnel@gmail.com>
2015-05-22 10:53:00 +0800

    update post method

--- svm/views.py
+++ svm/views.py
@@ -1,9 +1,13 @@
+import json
+import cPickle as pickle
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 
 from forms import ApplicationForm
 # Create your views here.
+
 
 class IndexView(TemplateView):
     form_class = ApplicationForm
@@ -14,4 +18,30 @@ class IndexView(TemplateView):
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
-         form = self.form_class(request.POST)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            X = []
+            X.append(str(form.cleaned_data['parents']))
+            X.append(str(form.cleaned_data['has_nurs']))
+            X.append(str(form.cleaned_data['form']))
+            X.append(str(form.cleaned_data['children']))
+            X.append(str(form.cleaned_data['housing']))
+            X.append(str(form.cleaned_data['finance']))
+            X.append(str(form.cleaned_data['social']))
+            X.append(str(form.cleaned_data['health']))
+            encoders = pickle.load(open('svm/pickles/encoders.pickle','r'))
+            for i in range(0, len(X)):
+                X[i] = encoders[i].transform([str(X[i])])[0]
+            svm = pickle.load(open('svm/pickles/svm.pickle','r'))
+            print X
+            prediction = svm.predict(X)
+            label = encoders[-1].inverse_transform(prediction)
+            return HttpResponse(
+                json.dumps({"prediction": label[0]}),
+                content_type="application/json"
+            )
+        else:
+            return HttpResponse(
+                json.dumps(form.errors),
+                content_type="application/json"
+            )
